971589d080e1bcca8c14e3d0d7f07c9f23a02e6f
takemei-iwa <takemei567@gmail.com>
2024-08-14 15:40:45 +0900

    ブログ投稿画面

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,6 @@ Route::get('/', [PostController::class, 'index']);
 
 Route::get('/posts', [PostController::class, 'index']);   
 
+Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
 // '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
